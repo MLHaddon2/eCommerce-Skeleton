600fb01bed581afa0123dcb6139e943bbfc5396c
MLHaddon2 <haddon.mike2@outlook.com>
2026-04-27 12:28:53 -0500

    Changed database details for deployment

--- eCommerce/config/Database.js
+++ eCommerce/config/Database.js
@@ -6,7 +6,7 @@ const db = new Sequelize(
   process.env.DB_PASSWORD || 'password',
   {
     host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   }
 );
